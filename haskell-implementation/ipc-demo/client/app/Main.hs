d03ee9499092d140ac59566858446b15380c705d
shinokun <hypatia.tile02021@gmail.com>
2026-01-05 00:49:40 +0900

    Add haskell equivalent demo project

---
+++ haskell-implementation/ipc-demo/client/app/Main.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.ByteString.Char8 as B
+import Network.Socket
+import Network.Socket.ByteString (recv, sendAll)
+import System.Environment (getArgs)
+
+socketPath :: FilePath
+socketPath = "../ipc.sock"
+
+main :: IO ()
+main = withSocketsDo $ do
+  args <- getArgs
+  let
+    msg = case args of
+      [] -> "hello\n"
+      (x : _) -> B.pack x <> "\n"
+
+  let addr = SockAddrUnix socketPath
+
+  sock <- socket AF_UNIX Stream defaultProtocol
+  connect sock addr
+
+  sendAll sock msg
+
+  reply <- recv sock 1024
+  B.putStr reply
+
+  close sock
---
+++ haskell-implementation/ipc-demo/server/app/Main.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Exception (bracket, finally)
+import Control.Monad (forever, void, when)
+import qualified Data.ByteString.Char8 as B
+import Network.Socket (
+  Family (AF_UNIX),
+  SockAddr (SockAddrUnix),
+  Socket,
+  SocketOption (ReuseAddr),
+  SocketType (Stream),
+  accept,
+  bind,
+  close,
+  defaultProtocol,
+  listen,
+  setSocketOption,
+  socket,
+  withSocketsDo,
+ )
+import Network.Socket.ByteString (recv, sendAll)
+import System.Directory (doesFileExist, removeFile)
+
+socketPath :: FilePath
+socketPath = "../ipc.sock"
+
+main :: IO ()
+main = withSocketsDo $ do
+  stale <- doesFileExist socketPath
+  when stale $ removeFile socketPath
+
+  let addr = SockAddrUnix socketPath
+
+  bracket (socket AF_UNIX Stream defaultProtocol) close $ \sock -> do
+    setSocketOption sock ReuseAddr 1
+    bind sock addr
+    listen sock 16
+
+    putStrLn "server listening on ./ipc.sock"
+
+    putStrLn "server listening on ./ipc.sock"
+
+    forever $ do
+      (conn, _peer) <- accept sock
+      void $ handdleClient conn
+
+handdleClient :: Socket -> IO ()
+handdleClient conn =
+  finally
+    ( do
+        -- Read "some bytes". We'll treat it as one line-ish message like the C version.
+        msg <- recv conn 1024
+        if B.null msg
+          then pure ()
+          else do
+            let out = B.concat ["ack: ", msg]
+            sendAll conn out
+    )
+    (close conn)

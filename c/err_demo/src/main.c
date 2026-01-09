4581fb680df24faeff9c79feccb32aed987d5dd7
shinokun <hypatia.tile02021@gmail.com>
2026-01-09 14:26:13 +0900

    Add example for err_demo
    Add examples of error numbers and their meanings

--- c/err_demo/src/main.c
+++ c/err_demo/src/main.c
@@ -1,8 +1,12 @@
-
 #include <errno.h>
 #include <stdio.h>
 
 int main(void) {
-  errno = ENOENT;
-  perror("test");
+  errno = ENOENT; // No such file or directory
+  perror("test ENOENT");
+  errno = EACCES; // Permission denied
+  perror("test EACCES");
+  errno = EINTR; // Interrupted system call
+  perror("test EINTR");
+  return 0;
 }

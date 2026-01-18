a39f06de8502623a4055c09717d2f024e64b2174
shinokun <hypatia.tile02021@gmail.com>
2026-01-18 21:36:22 +0900

    Modified Haskell demo for IPC with new library files and tests

--- haskell/ipc-demo/client/app/Main.hs
+++ haskell/ipc-demo/client/app/Main.hs
@@ -1,31 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
+module Main (main) where
 
-module Main where
-
-import qualified Data.ByteString.Char8 as B
-import Network.Socket
-import Network.Socket.ByteString (recv, sendAll)
-import System.Environment (getArgs)
-
-socketPath :: FilePath
-socketPath = "../ipc.sock"
+import qualified MyLib (someFunc)
 
 main :: IO ()
-main = withSocketsDo $ do
-  args <- getArgs
-  let
-    msg = case args of
-      [] -> "hello\n"
-      (x : _) -> B.pack x <> "\n"
-
-  let addr = SockAddrUnix socketPath
-
-  sock <- socket AF_UNIX Stream defaultProtocol
-  connect sock addr
-
-  sendAll sock msg
-
-  reply <- recv sock 1024
-  B.putStr reply
-
-  close sock
+main = do
+  putStrLn "Hello, Haskell!"
+  MyLib.someFunc
